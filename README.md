266d3e3ddbcbfc319fda7319e1e85a1710e9795c
Ihor Vyspiansky <ivyspianskyi@n-ix.com>
2017-10-11 17:11:34 +0300

    Basic steps

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # voting-redux-app
+
+To start the development web server use the command:
+
+```
+$ yarn start
+```
--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { voteAngular, voteReact, voteVuejs } from './actions'
 import './App.css';
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.store = this.props.store;
+  }
+  handleVoteAngular = () => {
+    this.store.dispatch(voteAngular());
+  }
+  handleVoteReact = () => {
+    this.store.dispatch(voteReact());
+  }
+  handleVoteVuejs = () => {
+    this.store.dispatch(voteVuejs());
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="jumbotron" style={{'textAlign': 'center'}}>
+          <h2>What is your favorite front-end development framework 2017?</h2>
+          <h4>Click on the texts below to vote!</h4>
+          <br />
+          <div className="row">
+            <div className="col-xs-offset-3 col-xs-2">
+              <span onClick={this.handleVoteAngular}>Angular</span>
+            </div>
+            <div className="col-xs-2">
+              <span onClick={this.handleVoteReact}>React</span>
+            </div>
+            <div className="col-xs-2">
+              <span onClick={this.handleVoteVuejs}>Vue.js</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
